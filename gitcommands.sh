a8ec81d1d0422fb7c48ac9226fe2c342eb05c4e4
Christian Marroquin <chrismadav@gmail.com>
2025-04-23 13:36:27 -0600

    Updating Readme.md

--- gitcommands.sh
+++ gitcommands.sh
@@ -1,6 +1,6 @@
 # Para guardar cambios en GitHub
 git add .
-git commit -M "Adding Git Commands"
+git commit -m "Adding Git Commands"
 git push 
 
 # Para recuperar los cambios hechos de máquina a máquina.  
